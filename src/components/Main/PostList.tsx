aa5b529fe9cc8f71aff3eb22281238f0ffa55db9
limhizy15 <limhizy15@ajou.ac.kr>
2022-01-01 15:44:31 +0900

    글 목록 출력

---
+++ src/components/Main/PostList.tsx
@@ -0,0 +1,36 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+import PostItem from 'components/Main/PostItem'
+
+const POST_ITEM_DATA = {
+  title: 'Post Item Title',
+  date: '2020.01.29.',
+  categories: ['Web', 'Frontend', 'Testing'],
+  summary:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugit quis rem temporibus! Maxime molestias, suntrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!',
+  thumbnail:
+    'https://images.unsplash.com/photo-1517404215738-15263e9f9178?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dXJsfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+  link: '<https://www.google.co.kr/>',
+}
+
+const PostListWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+  width: 768px;
+  margin: 0 auto;
+  padding: 50px 0 100px;
+`
+
+const PostList: FunctionComponent = function () {
+  return (
+    <PostListWrapper>
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+    </PostListWrapper>
+  )
+}
+
+export default PostList
