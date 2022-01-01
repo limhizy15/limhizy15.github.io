464ff422fc4a139e6e68b190a9225f3da81bcbc5
limhizy15 <limhizy15@ajou.ac.kr>
2022-01-01 15:51:27 +0900

    md를 위한 라이브러리 설치 & 세팅

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,2 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+import 'prismjs/themes/prism-tomorrow.css'
+// https://github.com/PrismJS/prism/tree/1d5047df37aacc900f8270b1c6215028f6988eb1/themes
